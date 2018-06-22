a0e29e80ed0cac646dc8d2be39407e25888d03d8
aifly <aifly_xc@163.com>
2018-06-22 18:53:56 +0800

    a

---
+++ assets/js/base.js
@@ -0,0 +1,110 @@
+﻿var imgs = {
+	play: './assets/images/play.png',
+	title: './assets/images/title.jpg',
+	point: './assets/images/point.png',
+	shanyaozhixing: './assets/images/shanyaozhixing.jpg',
+	qiumishenghui: './assets/images/qiumishenghui.jpg',
+	jingcaidingge: './assets/images/jingcaidingge.jpg',
+	ranqingqiuchang: './assets/images/ranqingqiuchang.jpg',
+	logo: './assets/images/logo.png',
+	sure: './assets/images/sure.png',
+	img1:"./assets/images/1-1.png",
+	img2:"./assets/images/1-2.png",
+	img3:"./assets/images/1-3.png",
+	img4:"./assets/images/1-4.png",
+	img5:"./assets/images/1-5.png",
+	img6:"./assets/images/1-6.png",
+	img7:"./assets/images/1-7.png",
+	img8:"./assets/images/1-8.png",
+
+	img9:"./assets/images/2-1.png",
+	img10:"./assets/images/2-2.png",
+	img11:"./assets/images/2-3.png",
+	img12:"./assets/images/2-4.png",
+	img13:"./assets/images/2-5.png",
+	img14:"./assets/images/2-6.png",
+	img15:"./assets/images/2-7.png",
+
+	img16: "./assets/images/3-1.png",
+	img17: "./assets/images/3-2.png",
+	img18: "./assets/images/3-3.png",
+	img19: "./assets/images/3-4.png",
+	img20: "./assets/images/3-5.png",
+	
+	img21: "./assets/images/4-1.png",
+	img22: "./assets/images/4-2.png",
+	img23: "./assets/images/4-3.png",
+	img24: "./assets/images/4-4.png",
+	img25: "./assets/images/4-5.png",
+	img26: "./assets/images/4-6.png",
+	img27: "./assets/images/4-7.png",
+	img28: "./assets/images/4-8.png",
+	img29: "./assets/images/4-8.png"
+
+}
+
+
+
+
+var arr = [];
+
+
+for (var attr in imgs) {
+	arr.push(imgs[attr]);
+}
+
+
+var mainImgList = [
+	{
+		url:imgs.img1,
+		type:'rect'
+	},{
+		url:imgs.img2,
+		type:'circle'
+	},{
+		url:imgs.img3,
+		type:'circle'
+	}
+];
+
+var musics = {
+	music: {
+		src: './assets/music/bg2.mp3',
+		autoplay: false,
+		name: 'bg',
+		loop: true
+	},
+	photo:{
+		src:'./assets/music/photo.mp3',
+		autoplay:false,
+		name:"photo",
+		loop:false
+	}
+};
+
+ (function () {
+
+ 	if (typeof WeixinJSBridge == "object" && typeof WeixinJSBridge.invoke == "function") {
+ 		handleFontSize();
+ 	} else {
+ 		if (document.addEventListener) {
+ 			document.addEventListener("WeixinJSBridgeReady", handleFontSize, false);
+ 		} else if (document.attachEvent) {
+ 			document.attachEvent("WeixinJSBridgeReady", handleFontSize);
+ 			document.attachEvent("onWeixinJSBridgeReady", handleFontSize);
+ 		}
+ 	}
+
+ 	function handleFontSize() {
+ 		// 设置网页字体为默认大小
+ 		WeixinJSBridge.invoke('setFontSizeCallback', {
+ 			'fontSize': 0
+ 		});
+ 		// 重写设置网页字体大小的事件
+ 		WeixinJSBridge.on('menu:setfont', function () {
+ 			WeixinJSBridge.invoke('setFontSizeCallback', {
+ 				'fontSize': 0
+ 			});
+ 		});
+ 	}
+ })();
---
+++ components/index/index.vue
@@ -0,0 +1,221 @@
+<template>
+	<div v-tap='[entry]'  class="lt-full zmiti-index-main-ui "  :class="{'show':show}" >
+		<transition name='index'>
+			<div class="zmiti-index lt-full" ref='page' v-if='!showIndexMask' :style="{height:viewH+'px'}">
+				<section>
+					<div>
+						<img :src="imgs.title" alt="">
+					</div>
+					<canvas :width="viewW" height="500" ref='canvas'>
+					</canvas>
+					<div class="zmiti-vote-group" v-for='(group,i) in dataSource' :key="i">
+						<header>
+							<img :src="group.groupimage" alt="">
+						</header>
+						<div v-for='(vote ,k ) in group.list' :key='k'  class="zmiti-vote-item" >
+							<div>
+								<div v-tap='[openImage,vote]' :style="{width:210+'px',height:'210px',background:'url('+vote.image+') no-repeat center',backgroundSize:'cover'}">
+								</div>
+								<div class="zmiti-vote-item-title" v-tap='[toggle,vote]'>
+									<div>{{vote.title}}</div>
+									<div>
+										<span :class="{'active':vote.checked}"></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="zmiti-sure">
+						<img :src="imgs.sure" alt="">
+					</div>
+				</section>
+			</div>
+		</transition>
+		<div  class="zmiti-image-mask lt-full" v-if='currentVote.image' v-tap='[closeMask]'>
+			<div>
+				<img :src="currentVote.image" alt="">
+			</div>
+			<div class="zmiti-vote-content">
+				<h2>{{currentVote.title}}</h2>
+				<div>{{currentVote.content}}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import './index.css';
+	import {imgs} from '../lib/assets.js';
+	import zmitiUtil from '../lib/util';
+	import Point from './point';
+	import IScroll from 'iscroll';
+	export default {
+		props:['obserable','nickname','pv'],
+		name:'zmitiindex',
+		data(){
+			return{
+				imgs,
+				pointW:0,
+				pointH:0,
+				points:[],
+				show:true,
+				dataSource:[],
+				showIndexMask:false,
+				showMasks:false,
+				transX:-200,
+				image:'',
+				currentVote:{},
+				showVideo:false,
+				viewW:window.innerWidth,
+				viewH:window.innerHeight,
+				fullscreen:true,
+			}
+		},
+		components:{
+		},
+		
+		methods:{
+
+			imgStart(e){
+				e.preventDefault(); 
+			},
+
+			openImage(vote){
+				
+				this.currentVote =  vote;
+				console.log(vote)
+			},
+			closeMask(){
+				this.currentVote = {};
+			},
+			toggle(vote){
+				vote.checked = !vote.checked;
+				this.dataSource = this.dataSource.concat([]);
+			},
+			submit(){
+				var voteids = '1,2,3,4,5,6'; //要投票的id集合
+				$.ajax({//提交投票
+					url:"xxxx",
+					type:'post',
+					data:{
+						voteids:voteids
+					}
+				});
+
+				$.ajax({//获取投票结果
+					type:'post',
+					url:'xxxx',
+				})
+			},
+			entry(){
+				
+				return;
+				var {obserable} = this;
+				this.showIndexMask = true;
+				this.showVideo = true;
+
+				setTimeout(()=>{
+					this.$refs['video'].addEventListener('play',()=>{
+		 				obserable.trigger({
+		 					type:"toggleBgMusic",
+		 					data:false
+		 				})
+		 			})
+
+		 			this.$refs['video'].addEventListener('ended',()=>{
+		 				/* obserable.trigger({
+		 					type:"toggleBgMusic",
+		 					data:true
+						 }); */
+						 
+						 obserable.trigger({
+							 type:'entryIntro'
+						 });
+		 			})
+
+		 			/* this.$refs['video'].addEventListener('pause',()=>{
+		 				this.show && this.$refs['video'].play()	
+					 }) */
+					 
+					 
+					this.$refs['video'].play()
+				},200)
+			},
+
+			initPoints(){
+
+				var canvas = this.$refs['canvas'];
+				var context = canvas.getContext('2d');
+
+				var width = canvas.width,
+					height = canvas.height;
+				var img = new Image();
+				img.onload = ()=>{
+					for(var i = 0 ;i<100;i++){
+						var p = new Point({
+							img,
+							context,
+						})
+						this.points.push(p);
+					}
+				}
+				img.src = imgs.point;
+
+				var animationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame,
+					m = Math;
+
+				var render = ()=>{
+					if(width<=0){
+						width = canvas.width,
+						height = canvas.height;
+					}
+					context.clearRect(0,0,width,height);
+
+					this.points.map((point,i)=>{
+						point.angle += point.speed;
+						//point.angle = (point.angle | 0)
+						point.angle %= 360;
+						point.x += m.sin(point.angle/180*m.PI)*point.speedX;
+
+						point.y -= 1;
+						if(point.defaultY  -  point.y  >  point.maxHeight){
+							point.y = point.defaultY
+						}
+						point.update();
+					});
+					!this.showIndexMask && animationFrame(render);
+				}
+				
+				render()
+
+
+			},
+			getData(){
+				var s = this;
+				$.getJSON('./assets/js/data.json?t='+new Date().getTime(),data=>{
+					if(data.code === 0){
+						s.dataSource = data.list;
+						setTimeout(() => {
+							s.scroll.refresh();
+						}, 300);
+					}
+				})
+			}
+			
+		},
+		mounted(){
+
+			var {obserable} = this;
+			this.getData();
+			obserable.on('toggleIndex',(data)=>{
+				this.show = data.show;
+			})
+
+			//this.initPoints();
+
+			this.scroll = new IScroll(this.$refs['page'],{
+				scrollbars:true
+			})
+		}
+	}
+</script>
